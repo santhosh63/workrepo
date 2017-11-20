3190d9f4ae75ceda23d961f07b1b84395451cd5a
santhosh <santhohnalla1993@gmail.com>
2017-11-20 11:38:03 -0500

    modifying jenkinsfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,7 +4,6 @@ node {
 
     stage ('start') {
        checkout scm
-       git 'https://github.com/santhosh63/workrepo.git'
         // Get the maven tool.
         // ** NOTE: This 'M3' maven tool must be configured
         // **       in the global configuration
